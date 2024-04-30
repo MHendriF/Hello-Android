818bd783b54faecf35342ff44766d2630c61a288
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-30 09:36:38 +0700

    send data using intent

--- Dicoding/Pemula/MyIntentApp/app/build.gradle.kts
+++ Dicoding/Pemula/MyIntentApp/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.mhendrif.myintentapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mhendrif.myintentapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

b3771c532ca1a4ea1aaa631dd8bc121522816df7
MHendriF <hendrifebriansyah28@gmail.com>
2024-04-30 12:00:03 +0700

    recyclerview

---
+++ Dicoding/Pemula/MyRecyclerView/app/src/main/java/com/mhendrif/myrecyclerview/model/Hero.kt
@@ -0,0 +1,11 @@
+package com.mhendrif.myrecyclerview.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    val name: String,
+    val description: String,
+    val photo: Int
+) : Parcelable

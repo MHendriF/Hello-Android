d5dc1008def4c6f3cf78f6858d78c15ef96bd182
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-05 10:17:56 +0700

    show list heroes using LazyColumn

--- Dicoding/Compose/JetHeroes/app/src/main/java/com/mhendrif/jetheroes/MainActivity.kt
+++ Dicoding/Compose/JetHeroes/app/src/main/java/com/mhendrif/jetheroes/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,28 +22,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetHeroesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        JetHeroesApp()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetHeroesTheme {
-        Greeting("Android")
     }
 }
